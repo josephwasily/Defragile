f65ce075f28bf311e35060d7bcc6decb5aa807aa
Joseph Botros <jwasily@hotmail.com>
2023-06-23 18:36:35 +0300

    add changes

---
+++ PrometheusNetSample/TestAPI/Controllers/ExperimentController.cs
@@ -0,0 +1,33 @@
+﻿using AntifragilePolicies.Polly;
+using Microsoft.AspNetCore.Mvc;
+using Polly;
+using Polly.CircuitBreaker;
+
+namespace PrometheusNetSample.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExperimentController : ControllerBase
+    {
+        private readonly ILogger<ExperimentController> _logger;
+        private readonly HttpClient _httpClient;
+
+        public ExperimentController(
+            ILogger<ExperimentController> logger,
+            IHttpClientFactory _clientFactor
+        )
+        {
+            _logger = logger;
+            _httpClient = _clientFactor.CreateClient("backendHttpClient");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            var result = await _httpClient.GetAsync("/");
+            return Ok(await result.Content.ReadAsStringAsync());
+
+        }
+
+    }
+}
